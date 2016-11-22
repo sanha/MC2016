ab4c85e7483a8a1769929887c18a43d2d7a24577
sanha <sanhaleehana@naver.com>
2016-11-22 19:48:08 +0900

    [HW6] matmul with OpenMP

---
+++ hw6/mat_mul.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include "timers.h"
+#ifdef _OPENMP
+	#include <omp.h>
+#endif
+
+int print_matrix = 0;
+int validation = 0;
+
+void mat_mul( float *c, float *a, float *b, int NDIM, int NTHD) {
+	int i, j, k;
+	
+	omp_set_num_threads(NTHD);
+
+	#pragma omp parallel for schedule(guided, 64)
+	for (i = 0; i < NDIM; i++) {
+		for (j = 0; j < NDIM; j++) {
+			for (k = 0; k < NDIM; k++) {
+				c[i * NDIM + k] += a[i * NDIM + j] * b[j * NDIM + k];
+			}
+		}
+	}
+
+	return ;
+}
+
+/************************** DO NOT TOUCH BELOW HERE ******************************/
+
+void check_mat_mul( float * c, float * a, float * b, int NDIM )
+{
+	int i, j, k, x, y;
+	float sum, result;
+	int validated = 1;
+
+	printf("Validating the result..\n");
+	
+  srand(time(NULL));
+	// C = AB
+	for( x = 0; x < 10; x++ )
+	{
+		for( y = 0; y < 10; y++ )
+		{
+      i = rand() % NDIM;
+      j = rand() % NDIM;
+			sum = 0;
+
+			for( k = 0; k < NDIM; k++ )
+			{
+				sum += a[i * NDIM + k] * b[k * NDIM + j];
+			}
+
+      result = c[i * NDIM + j];
+
+			if( result - sum > 0.001 || result - sum < -0.001 )
+			{
+				printf("c[%d][%d] is differ(value=%lf correct_value=%lf)!!\n", i, j, c[i * NDIM + j], sum );
+				validated = 0;
+			}
+		}
+	}
+
+	printf("Validation : ");
+	if( validated )
+		printf("SUCCESSFUL.\n");
+	else
+		printf("FAILED.\n");
+}
+
+void print_mat( float * mat, int NDIM )
+{
+	int i, j;
+
+	for( i = 0; i < NDIM; i++ )
+	{
+		for( j = 0; j < NDIM; j++ )
+		{
+			printf("%8.2lf ", mat[i * NDIM + j]);
+		}
+		printf("\n");
+	}
+}
+
+void print_help(const char* prog_name)
+{
+	printf("Usage: %s [NDIM] [NTHD] [-pvh]\n", prog_name );
+	printf("\n");
+	printf("OPTIONS\n");
+	printf("  -p : print matrix data.\n");
+	printf("  -v : validate matrix multiplication.\n");
+	printf("  -h : print this page.\n");
+}
+
+void parse_opt(int argc, char** argv)
+{
+	int opt;
+
+	while( (opt = getopt(argc, argv, "pvhikjs:")) != -1 )
+	{
+		switch(opt)
+		{
+		case 'p':
+			// print matrix data.
+			print_matrix = 1;
+			break;
+
+		case 'v':
+			// validation
+			validation = 1;
+			break;
+
+		case 'h':
+		default:
+			print_help(argv[0]);
+			exit(0);
+			break;
+		}
+	}
+}
+
+int main(int argc, char** argv)
+{
+
+  int i, j, k = 1;
+  int NDIM = 1024;
+  int NTHD = 1;
+  float * a, * b, * c;
+
+  NDIM = atoi(argv[1]);
+  NTHD = atoi(argv[2]);
+  parse_opt( argc, argv );
+
+  a = (float *)malloc(sizeof(float) * NDIM * NDIM);
+  b = (float *)malloc(sizeof(float) * NDIM * NDIM);
+  c = (float *)malloc(sizeof(float) * NDIM * NDIM);
+
+  printf("%d x %d x %d\n", NDIM, NDIM, NDIM);
+  printf("NTRD %d\n", NTHD);
+
+	for( i = 0; i < NDIM; i++ )
+	{
+		for( j = 0; j < NDIM; j++ )
+		{
+			a[i * NDIM + j] = k;
+			b[i * NDIM + j] = k;
+			k++;
+		}
+	}
+
+	timer_start(1);
+	mat_mul( c, a, b, NDIM, NTHD );
+	timer_stop(1);
+
+	printf("Time elapsed : %lf sec\n", timer_read(1));
+
+	if( validation )
+		check_mat_mul( c, a, b, NDIM );
+
+	if( print_matrix )
+	{
+		printf("MATRIX A: \n");
+		print_mat(a, NDIM);
+
+		printf("MATRIX B: \n");
+		print_mat(b, NDIM);
+
+		printf("MATRIX C: \n");
+		print_mat(c, NDIM);
+	}
+
+	return 0;
+	//cl_device_id *device = malloc(sizeof(cl_device_id) * device_num);
+}
